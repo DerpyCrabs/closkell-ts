e750b4789b7a88901814af47e86e9a63ba264e9e
DerpyCrabs <derpycrabs@gmail.com>
2022-08-28 09:16:21 +0300

    Fix some deno lint warnings

--- samples.test.ts
+++ samples.test.ts
@@ -1,8 +1,8 @@
 import { assert, assertObjectMatch } from 'https://deno.land/std@0.151.0/testing/asserts.ts'
-import { parseToAST } from './parsing.ts'
-import { expandMacros } from './macros.ts'
+import { ASTParsingError, parseToAST } from './parsing.ts'
+import { expandMacros, verifyNoMacros } from './macros.ts'
 import { intrinsics } from './intrinsics.ts'
-import { evaluateExpression } from './evaluation.ts'
+import { evaluateExpression, EvaluationError } from './evaluation.ts'
 import { EvalAST } from './types.ts'
 
 Deno.test('samples', async (t) => {
@@ -11,19 +11,19 @@ Deno.test('samples', async (t) => {
   await Promise.all(samples.map((s) => t.step(s.filename, () => testSample(s))))
 })
 
-function testSample({ source, expected }: { filename: string; source: string; expected: any }): void {
+function testSample({ source, expected }: { filename: string; source: string; expected: EvalAST }): void {
   const parsedAst = parseToAST(source)
-  assert('result' in parsedAst, JSON.stringify((parsedAst as any).error))
+  assert('result' in parsedAst, JSON.stringify((parsedAst as ASTParsingError).error))
   const macroExpandedAst = parsedAst
   // TODO enable macros after let implementation
   //   const macroExpandedAst = expandMacros(parsedAst.result, intrinsics)
   //   assert('result' in macroExpandedAst, JSON.stringify((macroExpandedAst as any).error))
-  const evaledAst = evaluateExpression(macroExpandedAst.result as EvalAST, intrinsics)
-  assert('result' in evaledAst, JSON.stringify((evaledAst as any).error))
+  const evaledAst = evaluateExpression(verifyNoMacros(macroExpandedAst.result), intrinsics)
+  assert('result' in evaledAst, JSON.stringify((evaledAst as EvaluationError).error))
   assertObjectMatch(evaledAst.result, expected)
 }
 
-async function getSamples(): Promise<{ filename: string; source: string; expected: any }[]> {
+async function getSamples(): Promise<{ filename: string; source: string; expected: EvalAST }[]> {
   const fileList = Deno.readDir('./samples')
   const results = []
   for await (const file of fileList) {
