20549e51cfc7c18bedfb54be5a02d793e282f43d
DerpyCrabs <derpycrabs@gmail.com>
2022-09-03 15:25:58 +0300

    Move spans to ast values

--- types.ts
+++ types.ts
@@ -6,62 +6,74 @@ export type Span = {
 export type PString = {
   kind: 'string'
   value: string
+  span: Span
 }
 export type PAtom = {
   kind: 'atom'
   value: string
+  span: Span
 }
 export type PNumber = {
   kind: 'number'
   value: number
+  span: Span
 }
 
 export type PList = {
   kind: 'list'
   value: ParserAST[]
+  span: Span
 }
 
 export type PVector = {
   kind: 'vector'
   value: ParserAST[]
+  span: Span
 }
 
 export type PMap = {
   kind: 'map'
   value: ParserAST[]
+  span: Span
 }
 
-export type ParserAST = (PString | PAtom | PNumber | PList | PVector | PMap) & { span: Span }
+export type ParserAST = PString | PAtom | PNumber | PList | PVector | PMap
 
 export type EString = {
   kind: 'string'
   value: string
+  span?: Span
 }
 export type EAtom = {
   kind: 'atom'
   value: string
+  span?: Span
 }
 export type ENumber = {
   kind: 'number'
   value: number
+  span?: Span
 }
 
 export type EList = {
   kind: 'list'
   value: EvalAST[]
   env?: Binding[]
+  span?: Span
 }
 
 export type EVector = {
   kind: 'vector'
   value: EvalAST[]
   env?: Binding[]
+  span?: Span
 }
 
 export type EMap = {
   kind: 'map'
   value: EvalAST[]
   env?: Binding[]
+  span?: Span
 }
 
 export type EFunction = {
@@ -69,48 +81,54 @@ export type EFunction = {
   body: EvalAST
   arguments: string[]
   env: Binding[]
+  span?: Span
 }
 
 export type EIntrinsicFunction = {
   kind: 'intrinsicFunction'
   value: (args: EvalAST[]) => { result: EvalAST } | { error: string; span?: Span }
-}
-
-export type EvalAST = (EString | EAtom | ENumber | EList | EVector | EMap | EFunction | EIntrinsicFunction) & {
   span?: Span
 }
+
+export type EvalAST = EString | EAtom | ENumber | EList | EVector | EMap | EFunction | EIntrinsicFunction
 
 export type Binding = { name: string; value: EvalAST }
 
 export type MString = {
   kind: 'string'
   value: string
+  span?: Span
 }
 export type MAtom = {
   kind: 'atom'
   value: string
+  span?: Span
 }
 export type MNumber = {
   kind: 'number'
   value: number
+  span?: Span
 }
 
 export type MList = {
   kind: 'list'
   value: MacrosAST[]
   env?: MacrosBinding[]
+  span?: Span
 }
 
 export type MVector = {
   kind: 'vector'
   value: MacrosAST[]
   env?: MacrosBinding[]
+  span?: Span
 }
 
 export type MMap = {
   kind: 'map'
   value: MacrosAST[]
   env?: MacrosBinding[]
+  span?: Span
 }
 
 export type MFunction = {
@@ -118,6 +136,7 @@ export type MFunction = {
   body: MacrosAST
   arguments: string[]
   env: MacrosBinding[]
+  span?: Span
 }
 
 export type MMacro = {
@@ -125,20 +144,9 @@ export type MMacro = {
   body: MacrosAST
   arguments: string[]
   env: MacrosBinding[]
-}
-
-export type MacrosAST = (
-  | MString
-  | MAtom
-  | MNumber
-  | MList
-  | MVector
-  | MMap
-  | MFunction
-  | EIntrinsicFunction
-  | MMacro
-) & {
   span?: Span
 }
+
+export type MacrosAST = MString | MAtom | MNumber | MList | MVector | MMap | MFunction | EIntrinsicFunction | MMacro
 
 export type MacrosBinding = { name: string; value: MacrosAST }
